746b8e2bee91c1bf534e94d0fa4029ee4fc84b3a
papiya dutta <papiyadutta448@gmail.com>
2026-04-11 23:51:51 +0530

    it is a another way to declare the function in c lanaguage specially

--- firstpractice.c
+++ firstpractice.c
@@ -1,14 +1,38 @@
 // write a function that prints namaste if user is indian & bonjour if the user is french
-void printregion(char* user){
+// void printregion(char* user){
    
-    if(user=="Indian"){
-        printf("Namaste");
-    }
-    else if(user=="French"){
-        printf("Bonjour");
-    }
+//     if(user=="Indian"){
+//         printf("Namaste");
+//     }
+//     else if(user=="French"){
+//         printf("Bonjour");
+//     }
+// }
+// int main(){
+//  char* user="French";
+//  printregion(user); 
+// }
+// in onther way
+#include <stdio.h>
+void  PrintI(){
+    printf("Namaste");
+}
+void  PrintF(){
+    printf("Bonjour");
 }
 int main(){
- char* user="French";
- printregion(user); 
+    char n;
+    printf("Enter any key for i & f:");
+    scanf("%c",&n);
+  if(n =='i'){
+    PrintI();
+  }
+  else if(n=='f'){
+   PrintF();
+  }
+
 }
+
+
+
+
