712d24a1389ef97bf0482b7c7738801e52cf47f4
papiya dutta <papiyadutta448@gmail.com>
2026-04-13 14:51:43 +0530

    print the sum of n natural numbers using recursion

--- firstpractice.c
+++ firstpractice.c
@@ -86,17 +86,83 @@
 
 // make a power of any interfer taken by the user using in buit function
 
-#include<stdio.h>
-#include<math.h>
-void printPower(int a,int b){
-  int power=pow(a,b);
-  printf("%d",power);
-}
-int main(){
-  int a,b;
-  printf("Enter any number for base  and exponent:");
-  scanf("%d",&a);
-  scanf("%d",&b);
-  printPower(a,b);
+// #include<stdio.h>
+// #include<math.h>
+// void printPower(int a,int b){
+//   int power=pow(a,b);
+//   printf("%d",power);
+// }
+// int main(){
+//   int a,b;
+//   printf("Enter any number for base  and exponent:");
+//   scanf("%d",&a);
+//   scanf("%d",&b);
+//   printPower(a,b);
 
+// }
+
+//write functions to calculate area of a square ,a circle and a rectangle
+// #include<stdio.h>
+// #include<math.h>
+// // calculate the square
+// void printS(int a){
+//   int square=a*a;
+//   printf("  for square %d\n",square);
+// }
+// // calculate the circle
+// void printC(int a){
+//   float circle=3.14*(a*a);
+//   printf(" for circle %.2f\n",circle);
+// }
+// //  for calculating rectangle 
+// void printR(int a,int b){
+//   int rectangle=a*b;
+//   printf(" for rectangle %d\n",rectangle);
+// }
+// int main(){
+// int a,b;
+// printf("enter any two number for calculating the square ,circle and also rectangle:");
+// scanf("%d",&a);
+// scanf("%d",&b);
+// printS(a);
+// printC(a);
+// printR(a,b);
+// }
+
+// recursion
+// print hello world for 10 time usng recursion
+// #include<stdio.h>
+// void printHello();
+// int main(){
+//   int count=10;
+//   printHello(count);
+// return 0;
+// }
+// void printHello(int count){
+//   //base case
+//   if(count==0){
+//     return;
+//   }
+//  printf("Hello World\n");
+//  printHello(count-1);
+// }
+// question no 30 
+// sum of n natural number using recursion method in c programming language
+#include<stdio.h>
+int printNaturalNumber(int number ,int sum);
+int main(){
+  int number=1;
+  int sum=0;
+  int result=printNaturalNumber(number,sum);
+  printf(" sum is %d",result);
+return 0;
+}
+int printNaturalNumber(int number,int sum){
+//base case
+if(number>10){
+  return sum;
+}
+  sum= sum+number;
+   return printNaturalNumber(number+1,sum);
+   
 }
