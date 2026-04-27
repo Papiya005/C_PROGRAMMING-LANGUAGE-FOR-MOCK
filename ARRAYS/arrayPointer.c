43975046010b1a24e44b044398aab86ec1637fac
papiya dutta <papiyadutta448@gmail.com>
2026-04-27 11:32:46 +0530

    taking parameter inside an function

--- ARRAYS/arrayPointer.c
+++ ARRAYS/arrayPointer.c
@@ -1,23 +1,39 @@
 //array is a pointer
-#include<stdio.h>
-//declare the prototype
-void printArrayElement(int arr[]);
-int main(){
-    int arr[5];
-    printArrayElement(arr);
+// #include<stdio.h>
+// //declare the prototype
+// void printArrayElement(int arr[]);
+// int main(){
+//     int arr[5];
+//     printArrayElement(arr);
    
-    return 0;
+//     return 0;
 
-}
-void printArrayElement(int arr[]){
-    //input
+// }
+// void printArrayElement(int arr[]){
+//     //input
     
-    for(int i=0;i<5;i++){
-        printf("%d index :",i);
-        scanf("%d",&arr[i]);
+//     for(int i=0;i<5;i++){
+//         printf("%d index :",i);
+//         scanf("%d",&arr[i]);
+//     }
+//     //output
+//     for(int i=0;i<5;i++){
+//     printf("%d index: %d\n",i,arr[i]);
+//     }
+// }
+// taking parameter inside an function
+
+#include<stdio.h>
+//prototype declare
+void printElement(int arr[],int n);
+int main(){
+    int arr[]={1,2,3,4,5,6};
+ printElement( arr, 6);
+    return 0;
+}
+void printElement(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
     }
-    //output
-    for(int i=0;i<5;i++){
-    printf("%d index: %d\n",i,arr[i]);
-    }
+
 }
