da05b57676867089983632c19ac9c4abaa1eb0af
papiya dutta <papiyadutta448@gmail.com>
2026-04-30 11:01:21 +0530

    string basic

---
+++ C_PROGRAMMING-LANGUAGE-FOR-MOCK/STRING/stringbasic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char name[]={'p','a','p','i','y','a',' ','d','u','t','t','a','\0'};
+    printf("%s\n",name);
+    char name1[]="lipika dutta";
+     printf("%s\n",name1);
+    char class[]="apna college"; 
+     printf("%s\n",class);       
+    return 0;
+
+}
