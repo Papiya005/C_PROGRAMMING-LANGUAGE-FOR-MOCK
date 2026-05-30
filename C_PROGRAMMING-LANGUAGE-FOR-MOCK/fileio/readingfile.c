4b949c2c7d18aef92c91da11c61030b0e36b0deb
papiya dutta <papiyadutta448@gmail.com>
2026-05-31 00:06:30 +0530

    reading from a file using r

---
+++ C_PROGRAMMING-LANGUAGE-FOR-MOCK/fileio/readingfile.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr=fopen("text.txt","r");
+    char ch;
+    fscanf(fptr,"%c",&ch);
+    printf("character =%c\n",ch);
+    //
+   
+    fscanf(fptr,"%c",&ch);
+    printf("character =%c\n",ch);
+//
+
+    fscanf(fptr,"%c",&ch);
+    printf("character =%c\n",ch);
+
+   
+    //
+   
+    fscanf(fptr,"%c",&ch);
+    printf("character =%c\n",ch);
+    //
+   
+    fscanf(fptr,"%c",&ch);
+    printf("character =%c\n",ch);
+     fclose(fptr);
+}
