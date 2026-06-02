71f5c76f5320f8e74fdb6f0092b202ef6d356560
papiya dutta <papiyadutta448@gmail.com>
2026-06-02 21:05:13 +0530

    EOF concepts

---
+++ C_PROGRAMMING-LANGUAGE-FOR-MOCK/fileio/eof.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr=fopen("text.txt","r");
+    char ch=fgetc(fptr);
+    while(ch!=EOF){
+        printf("%c",ch);
+        ch=fgetc(fptr);
+    }
+    printf("\n");
+    fclose(fptr);
+    return 0;
+}
